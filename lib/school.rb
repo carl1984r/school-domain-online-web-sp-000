33d69b80e7c62d0d68d9c23e5fa5f67a7a1daaff
Bry Cox <carl.cc@gmail.com>
2019-02-13 08:18:42 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,25 @@
 # code here!
+require 'pry'
+class School
+  attr_accessor :roster, :name
+  attr_writer :grade
+
+def initialize(school)
+  @school = school
+  @roster = {}
+end
+
+def grade(num)
+  @grade
+  roster[num]
+end
+
+def add_student(name, grade)
+  (roster[grade] ||= []) << name
+  end
+
+  def sort
+  roster.sort
+  roster.values.sort!
+  end
+end
